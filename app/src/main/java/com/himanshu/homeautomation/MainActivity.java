a07fa035c1c3b0d53301a24767d6059472a257e2
Himanshu Rawat <rawath54@gmail.com>
2018-12-21 22:40:15 +0530

    Minor Day

--- app/src/main/java/com/himanshu/homeautomation/MainActivity.java
+++ app/src/main/java/com/himanshu/homeautomation/MainActivity.java
@@ -1,11 +1,16 @@
 package com.himanshu.homeautomation;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -23,6 +28,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -49,7 +59,24 @@ public class MainActivity extends AppCompatActivity
     private Switch relay4Switch;
 
 
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
 
+    private double latitude;
+    private double longitude;
+
+    private double homeLatitude;
+    private double homeLongitude;
+
+    private boolean homeLocationSet = false;
+    private boolean homeLocationClicked = false;
+
+    private TextView homeLatitudeTV;
+    private TextView homeLongitudeTV;
+    private TextView latituteTV;
+    private TextView longitudeTV;
+    private TextView distanceInMeterTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +93,12 @@ public class MainActivity extends AppCompatActivity
         relay3Switch = findViewById(R.id.content_main_relay_3_switch);
         relay4Switch = findViewById(R.id.content_main_relay_4_switch);
 
+        homeLatitudeTV = findViewById(R.id.content_main_home_latitude_text_view);
+        homeLongitudeTV = findViewById(R.id.content_main_home_longitude_text_view);
+        latituteTV = findViewById(R.id.content_main_latitude_text_view);
+        longitudeTV = findViewById(R.id.content_main_longitude_text_view);
+        distanceInMeterTV = findViewById(R.id.content_main_distance_from_home_text_view);
+
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference();
 
@@ -77,9 +110,9 @@ public class MainActivity extends AppCompatActivity
         relay1Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     databaseReference.child("RELAY1").setValue(1);
-                }else{
+                } else {
                     databaseReference.child("RELAY1").setValue(0);
                 }
             }
@@ -88,9 +121,9 @@ public class MainActivity extends AppCompatActivity
         relay2Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     databaseReference.child("RELAY2").setValue(1);
-                }else{
+                } else {
                     databaseReference.child("RELAY2").setValue(0);
                 }
             }
@@ -99,9 +132,9 @@ public class MainActivity extends AppCompatActivity
         relay3Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     databaseReference.child("RELAY3").setValue(1);
-                }else{
+                } else {
                     databaseReference.child("RELAY3").setValue(0);
                 }
             }
@@ -110,9 +143,9 @@ public class MainActivity extends AppCompatActivity
         relay4Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     databaseReference.child("RELAY4").setValue(1);
-                }else{
+                } else {
                     databaseReference.child("RELAY4").setValue(0);
                 }
             }
@@ -121,27 +154,60 @@ public class MainActivity extends AppCompatActivity
 
         boolean isServicesOK = isServicesOK();
 
-        if(homeLocationImageView != null){
-            if(isServicesOK) {
+        if (homeLocationImageView != null) {
+            if (isServicesOK) {
                 homeLocationImageView.setOnClickListener(this);
             }
         }
 
-        if(homeLocationTextView != null){
-            if(isServicesOK) {
+        if (homeLocationTextView != null) {
+            if (isServicesOK) {
                 homeLocationTextView.setOnClickListener(this);
             }
         }
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationCallback = new LocationCallback(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onLocationResult(LocationResult locationResult) {
+                if(locationResult == null){
+                    return;
+                }
+                else{
+                    latitude = locationResult.getLastLocation().getLatitude();
+                    longitude = locationResult.getLastLocation().getLongitude();
+
+                    longitudeTV.setText("Longitude - "+longitude);
+                    latituteTV.setText("Latitude - "+latitude);
+
+                    if(!homeLocationClicked) {
+                        homeLatitude = latitude;
+                        homeLongitude = longitude;
+                    }
+
+                    Log.i("Locationxd","Latitude "+latitude+" Longitude "+longitude);
+                    Log.i("Locationxd", "Home Latitude "+homeLatitude+" Longitude "+homeLongitude);
+
+
+                    if(homeLocationSet){
+                        turnOffRelays(latitude,longitude);
+                    }
+                }
             }
-        });
+        };
+
+        locationRequest = LocationRequest.create();
+        locationRequest.
+                setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).
+                setInterval(2000).
+                setFastestInterval(2000).
+                setNumUpdates(10000);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback,null);
+        }
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -151,6 +217,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
     }
 
     @Override
@@ -211,9 +280,9 @@ public class MainActivity extends AppCompatActivity
     public void onClick(View v) {
         if(v != null){
             if(homeLocationImageView.getId() == v.getId()){
-                startActivity(new Intent(this,MapsActivity.class));
+                setHomeLocation();
             }else if(homeLocationTextView.getId() == v.getId()){
-                startActivity(new Intent(this,MapsActivity.class));
+                setHomeLocation();
             }
         }
     }
@@ -308,4 +377,41 @@ public class MainActivity extends AppCompatActivity
 
         }
     };
+
+    private void setHomeLocation(){
+        homeLocationSet = true;
+        homeLocationClicked = true;
+        homeLongitudeTV.setText("Longitude - "+homeLongitude);
+        homeLatitudeTV.setText("Latitude - "+homeLatitude);
+    }
+
+    private float computeDistance(double latitude,double longitude){
+        float[] distance = new float[2];
+        Location.distanceBetween(homeLatitude, homeLongitude, latitude, longitude, distance);
+        return distance[0];
+    }
+
+    private void turnOffRelays(double latitude,double longitude){
+        float distance = computeDistance(latitude,longitude);
+        distanceInMeterTV.setText(distance+"m");
+        Log.i("Locationxd","Distance " +distance);
+        if(distance > 10f){
+            homeLocationSet = false;
+            homeLocationClicked = false;
+            if(relay1Switch.isChecked()){
+                relay1Switch.setChecked(false);
+            }
+            if(relay2Switch.isChecked()){
+                relay2Switch.setChecked(false);
+            }
+            if(relay3Switch.isChecked()){
+                relay3Switch.setChecked(false);
+            }
+            if(relay4Switch.isChecked()){
+                relay4Switch.setChecked(false);
+            }
+        }
+    }
+
 }
+
